f1d813d9d9671a438b0e15f56e4d39a2cf7f4478
Rodrigo Ratto <97546475+rattom@users.noreply.github.com>
2022-01-20 17:06:06 -0300

    Add files via upload

---
+++ src/Computador.java
@@ -0,0 +1,18 @@
+
+public class Computador extends Jogador{
+    
+    public Computador(int jogador2){
+        super(jogador2);
+        System.out.println("Jogador 'Computador' criado!");
+    }
+    
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
